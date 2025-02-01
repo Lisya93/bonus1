49476eea5183d49b4aa304361afccac9f6a3526e
Neto test <anna_laz.ru@mail.ru>
2025-02-01 19:50:13 +0300

    src

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,9 +1,9 @@
 public class BmiService {
 
-    public int calculate (int weight, double height) {
-    double bmi = weight / Math.pow(height, 2);
-    return (int) bmi;
+    public int calculate(int weight, double height) {
+        double bmi = weight / Math.pow(height, 2);
+        return (int) bmi;
 
-}
+    }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
         BmiService service = new BmiService();
-
-
-        int bmi = service.calculate (83, 1.80);;
-
-
+        int bmi = service.calculate(83, 1.80);
+        ;
     }
 }
